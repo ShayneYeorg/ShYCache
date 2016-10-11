4afc80387467181b0be841e571620d7dc87220d0
ShayneYeorg <mercury10@qq.com>
2016-10-11 09:56:42 +0800

    建立项目

---
+++ ShYCache/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShYCache
+//
+//  Created by 杨淳引 on 16/10/11.
+//  Copyright © 2016年 shayneyeorg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
